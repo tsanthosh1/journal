7612e995359ebd125c9b90000c1d24aed9fdb788
Santhosh Thamaraiselvan <tsanthosh.bk1@gmail.com>
2026-08-31 01:50:12 +0530

    fix(android): add Compose compiler gradle plugin for Kotlin 2.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
     id("com.google.gms.google-services")
 }
 
@@ -39,9 +40,6 @@ android {
     }
     buildFeatures {
         compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.14"
     }
     packaging {
         resources {
