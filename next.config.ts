89449c78f1b79e037b88d631154d0c32641203bd
Santhosh Thamaraiselvan <tsanthosh.bk1@gmail.com>
2026-08-31 00:34:04 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
+  serverExternalPackages: ["firebase-admin"],
   turbopack: {
     root: process.cwd(),
   },
